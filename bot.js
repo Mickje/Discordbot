548eb24ce30cd4efee15cc08d8580b7729d1860f
Mick <40500296+Mickje@users.noreply.github.com>
2018-10-30 21:16:01 +0200

    o
    o

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => { client.user.setGame('Happybday Elias!'); });
-    console.log('I am ready!'); 
-    });
 
 client.on('message', message => {
     if (message.content === '!stats') {
