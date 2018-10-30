31f02e48b3e488cd656247479e916145613f0f88
Mick <40500296+Mickje@users.noreply.github.com>
2018-10-30 20:38:26 +0200

    wow
    o

--- bot.js
+++ bot.js
@@ -7,31 +7,31 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!stats') {
-    	message.channel.send('https://cdn.battlemetrics.com/b/standardVertical/2443908.png?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%2336393f&chart=&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight=300');
+    	message.channel.send('https://cdn.battlemetrics.com/b/standardVertical/2443908.png?foreground=%23EEEEEE&linkColor=%231185ec&lines=%23333333&background=%2336393f&chart=&chartColor=%23FF0700&showPlayers=1&maxPlayersHeight=300').then(m => m.delete(10000));
     }
  });
 
 client.on('message', message => {
     if (message.content === '!wipe') {
-    	message.reply('The next wipe is on: 1.11.18 around 3PM EST');
+    	message.reply('The next wipe is on: 1.11.18 around 3PM EST').then(m => m.delete(10000));
   	}
 });
 
 client.on('message', message => {
     if (message.content === '!betaal') {
-    	message.channel.send('https://paypal.me/mickje');
+    	message.channel.send('https://paypal.me/mickje').then(m => m.delete(10000));
   	}
 });
 
 client.on('message', message => {
     if (message.content === '!factuur') {
-    	message.channel.send('https://digispark.saintware.club/files/leukefoto.png');
+    	message.channel.send('https://digispark.saintware.club/files/leukefoto.png').then(m => m.delete(10000));
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'gefeliciteerd') {
-    	message.channel.send('Happy birthday Elias!!🎉🎊').then(m => m.delete(0));
+    	message.channel.send('Happy birthday Elias!!🎉🎊').then(m => m.delete(10000));
   	}
 });
 
